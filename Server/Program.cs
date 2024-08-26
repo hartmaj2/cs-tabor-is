90a87c37aea8b690b98998543f6ecace3b94f33c
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-08-26 22:26:11 +0200

    Add necessary line

--- Server/Program.cs
+++ Server/Program.cs
@@ -5,7 +5,7 @@ builder.Services.AddControllers(); // This adds all classes marked with [ApiCont
 var app = builder.Build();
 
 app.UseBlazorFrameworkFiles(); // This is necessary so the server knows it should use the Blazor files in Client
-app.UseStaticFiles();
+app.UseStaticFiles(); // This is also necessary so Client Blazor can use the wwwroot static files
 
 app.MapControllers(); // This adds the controller services to our routes
 
