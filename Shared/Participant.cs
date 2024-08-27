ba692b31078638023a94a1e6f0a97e75132a285b
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-08-27 13:33:56 +0200

    Add key attribute and annotation for Entity

--- Shared/Participant.cs
+++ Shared/Participant.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Shared;
 
 public class Participant
 {
+    [Key]
+    public int Id {get; set; }
     public required string FirstName { get; init; }
     public required string LastName { get; init; }
     public required int Age { get; init; }
