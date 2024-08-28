70127414fc14c8faa993daf3b7eb7f4faacf70cb
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-08-28 09:02:27 +0200

    Remove unnecessary imports

--- Client/ViewModels/ParticipantFormData.cs
+++ Client/ViewModels/ParticipantFormData.cs
@@ -1,9 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-using System.Reflection.Metadata;
-using System.Runtime.ConstrainedExecution;
-using System.Text.Json.Serialization;
-
-
 
 public class ParticipantFormData
 {
