012ca24f84d67017f5b8db94b97b9cb69b15b51b
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-08-28 11:50:00 +0200

    Fix comment

--- Client/ViewModels/ParticipantFormData.cs
+++ Client/ViewModels/ParticipantFormData.cs
@@ -34,7 +34,7 @@ public class DivisibleByAttribute : ValidationAttribute
     }
 
     // Here for some reason the form was not validating properly when using ValidationResult overload of the IsValid method
-    // If I returned new ValidationResult with an error message, the form still allowed a submit even though the error message was displayed
+    // If I returned new ValidationResult with an error message, the form still showed the box as green even though the error message was displayed
     // That is the reason I am using bool IsValid override instead
     public override bool IsValid(object? value)
     {
