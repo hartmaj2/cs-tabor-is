09569819618041c11d35dc602f2c62257c472f53
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-08-30 08:14:12 +0200

    Add DbSets to the DbContext

--- Server/Data/DbContext.cs
+++ Server/Data/DbContext.cs
@@ -12,5 +12,17 @@ namespace Server.Data
 
         // DbSet is a substitute for my Participants SQL Table, which contains entries of type Participant (attributes = fields of Participant.cs model)
         public DbSet<Participant> Participants { get; set; }
+        public DbSet<Meal> Meals { get; set; }
+        public DbSet<Allergen> Allergens { get; set; }
+        public DbSet<MealAllergen> MealAllergens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<MealAllergen>()
+            .HasKey(ma => new { ma.MealId, ma.AllergenId });
+
+    }
     }
 }
