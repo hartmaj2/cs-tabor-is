87f7b7c4bef34a340881c29618a85b06e98cc9ce
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-08-30 15:45:16 +0200

    Implement http requests without having to specify id of the allergens

--- Server/Controllers/MealController.cs
+++ Server/Controllers/MealController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Shared;
 using Server.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
 
 
 [ApiController]
@@ -20,17 +22,52 @@ public class MealsController : ControllerBase
 
     // Gets all meals from the Meals table
     [HttpPost("add")]
-    public IActionResult AddMeal([FromBody] Meal meal)
+    public IActionResult AddMeal([FromBody] MealCreateDto received)
     {
+
+
+        var meal = new Meal
+        {
+            Name = received.Name,
+            MealTime = received.MealTime,
+            Type = received.Type,
+            Date = received.Date,
+            // MealAllergens = received.Allergens.Select(a => new MealAllergen
+            // {
+            //     AllergenId = a.Id
+            // }).ToList()
+        };
+
+        meal.MealAllergens = new List<MealAllergen>();
+        foreach (AllergenDto allergenDto in received.Allergens)
+        {
+            Allergen allergen = _context.Allergens.FirstOrDefault(a => a.Name == allergenDto.Name);
+            meal.MealAllergens.Add(new MealAllergen {AllergenId = allergen.Id});
+        }
+
         _context.Meals.Add(meal);
         _context.SaveChanges();
-        return CreatedAtAction(nameof(GetAllMeals),meal);
+        return CreatedAtAction(nameof(GetAllMeals),received);
     }
 
     // Gets the list of all meals from the meals table
+    // I have to map all Meal objects to MealCreateDtos because I want to be getting a list of all allergens for every meal
     [HttpGet("all")]
-    public IEnumerable<Meal> GetAllMeals()
+    public IEnumerable<MealCreateDto> GetAllMeals()
     {
-        return _context.Meals.ToList<Meal>();
+        return _context.Meals
+            .Select( meal => new MealCreateDto
+                {
+                    Name = meal.Name,
+                    MealTime = meal.MealTime,
+                    Type = meal.Type,
+                    Date = meal.Date,
+                    Allergens = _context.MealAllergens
+                        .Where(ma => ma.MealId == meal.Id)
+                        .Select(ma => ma.Allergen.ToAllergenDto())
+                        .ToList<AllergenDto>(),
+                }
+        );
     }
+
 }
