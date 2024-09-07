60e12731f04adecc8e25c0d50e2415623faaa3cb
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-09-07 09:01:16 +0200

    Add comments

--- Shared/Meal.cs
+++ Shared/Meal.cs
@@ -8,20 +8,20 @@ public class Meal
 
     [Required]
     [MaxLength(100)]
-    public required string Name { get; set; }
+    public required string Name { get; set; } // Name of the meal
 
     [Required]
     [JsonConverter(typeof(JsonStringEnumConverter))]
-    public required MealTime MealTime { get; set; }
+    public required MealTime MealTime { get; set; } // This is either Lunch or Dinner
 
     [Required]
     [JsonConverter(typeof(JsonStringEnumConverter))]
-    public required MealType Type { get; set; }
+    public required MealType Type { get; set; } // Either Soup or Main
 
     [Required]
-    public required DateOnly Date { get; set; }
+    public required DateOnly Date { get; set; } // The date when the food will be served
 
-    public ICollection<MealAllergen>? MealAllergens { get; set; }
+    public ICollection<MealAllergen>? MealAllergens { get; set; } // Represents allergens that are present in the food
 
 }
 
