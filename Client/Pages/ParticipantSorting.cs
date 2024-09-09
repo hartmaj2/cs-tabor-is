e65fb60a469ef6b2958ff16f6c86ae0c73f9721d
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-09-09 16:14:13 +0200

    Implement sorting by the diets

--- Client/Pages/ParticipantSorting.cs
+++ Client/Pages/ParticipantSorting.cs
@@ -1,11 +1,53 @@
-public class StringSwitchableComparer : ISwitchableComparer<string>
-{   
+using System.Text;
+
+// Abstract class det defines common behavior of all switchable comparers that can compare general objects
+public abstract class ObjectSwitchableComparer : ISwitchableComparer<object>
+{
     public bool ReverseSort { get; set; }
+
+    protected int GetDirectionInt()
+    {
+        return ReverseSort ? -1 : 1;
+    }
+
+    public virtual int Compare(object? x, object? y)
+    {
+         return x!.ToString()!.CompareTo(y!.ToString()!) * GetDirectionInt();
+    }
+}
+
+public class StringSwitchableComparer : ObjectSwitchableComparer, ISwitchableComparer<string>
+{   
     
     public int Compare(string? x, string? y)
     {
-        var directionInt = ReverseSort ? -1 : 1;
-        return x!.CompareTo(y) * directionInt;
+        return x!.CompareTo(y) * GetDirectionInt();
+    }
+
+    // This is kind of workaround because I couldn't have one collection of participant sorters witch comparers for different types 
+    public override int Compare(object? x, object? y)
+    {
+        return Compare((string)x!,(string)y!);
+    }
+}
+
+public class DietsSwitchableComparer : ObjectSwitchableComparer, ISwitchableComparer<IEnumerable<AllergenDto>>
+{
+
+    // converts the list of diets into a string and compares those strings
+    public int Compare(IEnumerable<AllergenDto>? dietsX, IEnumerable<AllergenDto>? dietsY)
+    {
+        StringBuilder builderX = new StringBuilder();
+        StringBuilder builderY = new StringBuilder();
+        foreach (var name in dietsX!.Select(allergen => allergen.Name)) builderX.Append(name);
+        foreach (var name in dietsY!.Select(allergen => allergen.Name)) builderY.Append(name);
+        return builderX.ToString().CompareTo(builderY.ToString()) * GetDirectionInt();
+    }
+
+    // This is kind of workaround because I couldn't have one collection of participant sorters witch comparers for different types 
+    public override int Compare(object? x, object? y)
+    {
+        return Compare((IEnumerable<AllergenDto>)x!,(IEnumerable<AllergenDto>)y!);
     }
 }
 
@@ -14,6 +56,7 @@ public interface ISwitchableComparer<T> : IComparer<T>
     public bool ReverseSort { get; set; }
 }
 
+// General Participant Sorter that can return a KeySelector and a KeyComparer
 public class ParticipantSorter<T>
 {
     public required Func<ParticipantDto,T> KeySelector { get; set;}
