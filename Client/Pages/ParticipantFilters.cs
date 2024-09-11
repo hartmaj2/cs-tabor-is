c349c553a22d9d23c27724c50ecf0b791d6a1bb7
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-09-11 09:08:19 +0200

    Change participant filters to queryable participant filters

--- Client/Pages/ParticipantFilters.cs
+++ Client/Pages/ParticipantFilters.cs
@@ -1,29 +1,29 @@
 using Shared;
 
 // This interface serves as a base for filters that I would like to apply to my participants table
-public interface IQueryableParticipantFilter
+public interface IParticipantFilter
 {
-    public IQueryable<Participant> GetFiltered(IQueryable<Participant> unfiltered);
+    public IEnumerable<ParticipantDto> GetFiltered(IEnumerable<ParticipantDto> unfiltered);
 }
 
 // The filtering is done by binding the filtering input field to the FilterText field of this filter
-public class QueryableTextFilter : IQueryableParticipantFilter
+public class TextFilter : IParticipantFilter
 {
     public string? FilterText { get ; set; }
 
-    public IQueryable<Participant> GetFiltered(IQueryable<Participant> unfiltered)
+    public IEnumerable<ParticipantDto> GetFiltered(IEnumerable<ParticipantDto> unfiltered)
     {
         if (string.IsNullOrWhiteSpace(FilterText)) return unfiltered;
         return unfiltered.Where(p => p.LastName!.Contains(FilterText,StringComparison.CurrentCultureIgnoreCase));
     }
 }
 
-public class QueryableIntegerBoundFilter : IQueryableParticipantFilter
+public class IntegerBoundFilter : IParticipantFilter
 {
     public int Min { get; set; }
     public int Max { get; set; }
 
-    public IQueryable<Participant> GetFiltered(IQueryable<Participant> unfiltered)
+    public IEnumerable<ParticipantDto> GetFiltered(IEnumerable<ParticipantDto> unfiltered)
     {
         return unfiltered.Where(p => p.Age >= Min && p.Age <= Max);
     }
