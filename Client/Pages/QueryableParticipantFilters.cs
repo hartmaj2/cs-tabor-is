10c86b207ca1bc68c6307fcd5fd30bc88392f29c
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-09-14 14:31:39 +0200

    Work with ParticipantDto instead of Participant

--- Client/Pages/QueryableParticipantFilters.cs
+++ Client/Pages/QueryableParticipantFilters.cs
@@ -3,7 +3,7 @@ using Shared;
 // This interface serves as a base for filters that I would like to apply to my participants table
 public interface IQueryableParticipantFilter
 {
-    public IQueryable<Participant> GetFiltered(IQueryable<Participant> unfiltered);
+    public IQueryable<ParticipantDto> GetFiltered(IQueryable<ParticipantDto> unfiltered);
 }
 
 // The filtering is done by binding the filtering input field to the FilterText field of this filter
@@ -11,7 +11,7 @@ public class QueryableTextFilter : IQueryableParticipantFilter
 {
     public string? FilterText { get ; set; }
 
-    public IQueryable<Participant> GetFiltered(IQueryable<Participant> unfiltered)
+    public IQueryable<ParticipantDto> GetFiltered(IQueryable<ParticipantDto> unfiltered)
     {
         if (string.IsNullOrWhiteSpace(FilterText)) return unfiltered;
         return unfiltered.Where(p => p.LastName!.Contains(FilterText,StringComparison.CurrentCultureIgnoreCase));
@@ -23,7 +23,7 @@ public class QueryableIntegerBoundFilter : IQueryableParticipantFilter
     public int Min { get; set; }
     public int Max { get; set; }
 
-    public IQueryable<Participant> GetFiltered(IQueryable<Participant> unfiltered)
+    public IQueryable<ParticipantDto> GetFiltered(IQueryable<ParticipantDto> unfiltered)
     {
         return unfiltered.Where(p => p.Age >= Min && p.Age <= Max);
     }
