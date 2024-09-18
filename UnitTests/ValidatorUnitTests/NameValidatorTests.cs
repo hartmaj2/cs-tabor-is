47450a8fdc78e4417292141c4052e3a9c0c7ddd8
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-09-18 14:06:42 +0200

    Add name validator unit tests

---
+++ UnitTests/ValidatorUnitTests/NameValidatorTests.cs
@@ -0,0 +1,53 @@
+namespace UnitTests;
+
+public class NameValidatorTests
+{
+    private static ValidNameAttribute nameValidator = new("first name");
+
+    [Theory]
+    [InlineData("Jiří")]
+    [InlineData("Strauß")]
+    [InlineData("Søren")]
+    [InlineData("Çağlar")]
+    [InlineData("Göran")]
+    public void CorrectNamesWithLanguageSpecificLetters(string name)
+    {
+        Assert.True(nameValidator.IsValid(name));
+    }
+
+    [Theory]
+    [InlineData("Dr. Etker")]
+    [InlineData("Karel IV.")]
+    [InlineData("Jane-Anne")]
+    [InlineData("O'Brian")]
+    public void CorrectNamesWithPunctuation(string name)
+    {
+        Assert.True(nameValidator.IsValid(name));
+    }
+
+    [Theory]
+    [InlineData("Karel 4")]
+    [InlineData("Mandalore195")]
+    [InlineData("42")]
+    public void IncorrectContainsDigit(string name)
+    {
+        Assert.False(nameValidator.IsValid(name));
+    }
+
+    [Theory]
+    [InlineData("karel@mail")]
+    [InlineData("nekdo,dulezity")]
+    [InlineData("(jelito)")]
+    public void IncorrectContainsPunctuation(string name)
+    {
+        Assert.False(nameValidator.IsValid(name));
+    }
+
+    [Theory]
+    [InlineData("anicka+matyasek")]
+    [InlineData("prom = deset")]
+    public void IncorrectContainsSymbols(string name)
+    {
+        Assert.False(nameValidator.IsValid(name));
+    }
+}
