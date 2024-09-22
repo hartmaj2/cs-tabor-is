89e07594347b7bc542841dda6a8e13ebdea788bf
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-09-22 16:18:59 +0200

    Add comments

--- Client/Pages/MealSorting.cs
+++ Client/Pages/MealSorting.cs
@@ -1,5 +1,9 @@
+// Comparer used to sort meals based on meal type (soup,main) first and then by name
 public class MealComparer : IComparer<MealDto>
 {
+
+    // Used so I don't have to create a new instance each time I am sorting inside some component
+    // I can do this because the MealComparer instances don't have any internal state
     public static MealComparer Instance { get; } = new MealComparer();
 
     public int Compare(MealDto? x, MealDto? y)
