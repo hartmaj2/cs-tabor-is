97f11417d64d1c7b4d9842877fb736c023076edc
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-09-22 19:47:19 +0200

    Add comments

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,12 +3,13 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<App>("#app"); // adds the App.razor component as a root component to the Blazor application (renders it inside the div with #app css selector)
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Scoped services live throughout one user connection (a new instance of the service is created only when the page is reloaded)
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<AllergenService>(); // add my allergen service so I can inject it to components that need it
-builder.Services.AddScoped<MealService>();
+builder.Services.AddScoped<AllergenService>(); // add my allergen service so I can pass list of all possible allergens to components that inject this service
+builder.Services.AddScoped<MealService>();  // similar to allergen service above
 builder.Services.AddBlazorBootstrap(); // add service for BlazorBootstrap needed for modals and dropdowns
 
 await builder.Build().RunAsync();
