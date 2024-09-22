126cf1e38d0d73b40055e26e184aa6c716490e6a
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-09-22 19:55:14 +0200

    Expand comment

--- Client/Program.cs
+++ Client/Program.cs
@@ -3,7 +3,7 @@ using Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app"); // adds the App.razor component as a root component to the Blazor application (renders it inside the div with #app css selector)
-// builder.RootComponents.Add<HeadOutlet>("head::after"); // adding the HeadOutlet seems not necessary
+// builder.RootComponents.Add<HeadOutlet>("head::after"); // adding the HeadOutlet seems not necessary (used for dynamically changing title or meta tags in head which i don't need)
 
 // Scoped services live throughout one user connection (a new instance of the service is created only when the page is reloaded)
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
