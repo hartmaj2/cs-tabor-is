6943377afca2965d2bdacbb23f618de760ae57c4
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-09-22 20:10:45 +0200

    Fix comments

--- Server/Controllers/AllergenController.cs
+++ Server/Controllers/AllergenController.cs
@@ -2,29 +2,29 @@ using Microsoft.AspNetCore.Mvc;
 using Server.Data;
 using Microsoft.EntityFrameworkCore;
 
-
 [ApiController]
 [Route("api/allergens")]
 
+// Used to handle requests related to allergens/diets that the meals/participants can contain/have
 public class AllergensController : ControllerBase
 {
 
     private readonly TaborIsDbContext _context;
 
-    // The context gets injected using dependency injection
+    // The context gets injected automatically using dependency injection
     public AllergensController(TaborIsDbContext context)
     {
         _context = context;
     }
 
-    // Gets the list of all meals from the meals table
+    // Gets the list of all allergens from the Allergens table
     [HttpGet("all")]
     public IEnumerable<AllergenDto> GetAllAllergens()
     {
         return _context.Allergens.ToList().Select(allergen => allergen.ToAllergenDto());
     }
 
-    // Gets the list of all meals from the meals table
+    // Adds an allergen to the Allergens table
     [HttpPost("add")]
     public IActionResult AddNewAllergen([FromBody] AllergenDto allergen)
     {
@@ -33,7 +33,7 @@ public class AllergensController : ControllerBase
         return CreatedAtAction(nameof(GetAllAllergens),allergen);
     }
 
-    // Adds a whole list of allergens
+    // Adds a whole list of allergens to the Allergens table (useful when populating empty table)
     [HttpPost("add-many")]
     public IActionResult AddMultipleAllergens([FromBody] ICollection<AllergenDto> allergens)
     {
@@ -42,7 +42,7 @@ public class AllergensController : ControllerBase
         return CreatedAtAction(nameof(GetAllAllergens),allergens);
     }
 
-    // Deletes single allergen with given id
+    // Deletes single allergen with given id from the Allergens table
     [HttpDelete("delete/{id:int}")]
     public IActionResult DeleteAllergen(int id)
     {
@@ -52,7 +52,8 @@ public class AllergensController : ControllerBase
         return NoContent();
     }
 
-    // Deletes everything from the allergens table
+    // Deletes everything from the Allergens table 
+    // (this is better than truncating because it takes foreign keys into account and uses cascading to remove the corresponding values in associative tables)
     [HttpDelete("delete-all")]
     public IActionResult DeleteAllAllergens()
     {
