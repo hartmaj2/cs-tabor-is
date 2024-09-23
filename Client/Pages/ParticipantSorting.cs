c7f925079e3dc73b6a283697f83c5c3ce56332a9
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-09-23 16:29:40 +0200

    Reorganize classes inside the file

--- Client/Pages/ParticipantSorting.cs
+++ Client/Pages/ParticipantSorting.cs
@@ -1,5 +1,87 @@
 using System.Text;
 
+// Used by div tables to sort the columns and track the corresponding arrows that should be displayed next to the active sorting column
+public class ColumnSortingManager
+{
+    private ParticipantSorter<object>[] _participantSorters;
+
+    private string[] _headerArrows;
+
+    // Getter only property that makes it impossible to assign to the backing field from the outside of this class
+    public IReadOnlyList<string> HeaderArrows => Array.AsReadOnly(_headerArrows);
+
+    // Current index by which we are sorting
+    private int activeColumnIndex;
+
+    // Constructor takes: 
+    //  1. the array of all sorters (combinations of key selectors and key comparers that can reverse direction) 
+    //  2. index of column which I want to have sorted on page initialization
+    public ColumnSortingManager(ParticipantSorter<object>[] participantSorters, int initialActiveColumnIndex)
+    {
+        _participantSorters = participantSorters;
+        activeColumnIndex = initialActiveColumnIndex;
+        _headerArrows = new string[_participantSorters.Length];
+    }
+
+    // The sort click is handled depending if the clicked column was already active or not
+    // we flip the sort direction if the column was already active and was clicked
+    // otherwise we sort in unreversed direction
+    // lastly we have to update our active column
+    public void HandleSortClick(int clickedColumn)
+    {
+        AdjustSorters(clickedColumn);
+        AdjustHeaderArrows(clickedColumn);
+        activeColumnIndex = clickedColumn;
+    }
+
+    public IEnumerable<ParticipantDto> GetSortedParticipants(IEnumerable<ParticipantDto> unsortedParticipants)
+    {
+        return unsortedParticipants.OrderBy(GetActiveSorterKeySelector(),GetActiveSorterKeyComparer());
+    }
+
+    private Func<ParticipantDto,object> GetActiveSorterKeySelector()
+    {
+        return _participantSorters[activeColumnIndex].KeySelector;
+    }
+
+    private ISwitchableComparer<object> GetActiveSorterKeyComparer()
+    {
+        return _participantSorters[activeColumnIndex].KeyComparer;
+    }
+
+    // Adjust the sorter reversed flags based on if we clicked on a new column or not
+    private void AdjustSorters(int clickedColumn)
+    {
+        if (clickedColumn == activeColumnIndex) // clicked on column that was already active
+        {
+            _participantSorters[clickedColumn].KeyComparer.ReverseSort = !_participantSorters[clickedColumn].KeyComparer.ReverseSort;
+        }
+        else // clicked on new column
+        {
+            _participantSorters[clickedColumn].KeyComparer.ReverseSort = false;
+        }
+    }
+
+    // Adjust the markers showing if sorting ascending or descending
+    private void AdjustHeaderArrows(int clickedColumn)
+    {
+        if (clickedColumn != activeColumnIndex) // clicked on a new column, remove the marker from the old one
+        {
+            _headerArrows[activeColumnIndex] = "";
+        }
+        _headerArrows[clickedColumn] = _participantSorters[clickedColumn].KeyComparer.ReverseSort ? "▼" : "▲"; // set current marker depending on if the sorter is reversed
+    }
+}
+
+// General Participant Sorter, can return a KeySelector (what property of participant we want to use for sorting) and a KeyComparer (how do we decide on the comparison result)
+// Cannot be an interface because we want the properties to be required on initialization
+public class ParticipantSorter<T>
+{
+    public required Func<ParticipantDto,T> KeySelector { get; set;}
+    public required ISwitchableComparer<T> KeyComparer { get; set;}
+
+}
+
 // Defines common behavior of all switchable comparers that can compare on given type
 public interface ISwitchableComparer<T> : IComparer<T>
 {
@@ -100,89 +182,5 @@ public class DietsSwitchableComparer : ObjectSwitchableComparer
     public override int Compare(object? x, object? y)
     {
         return Compare((IEnumerable<AllergenDto>)x!,(IEnumerable<AllergenDto>)y!);
-    }
-}
-
-
-
-// General Participant Sorter, can return a KeySelector (what property of participant we want to use for sorting) and a KeyComparer (how do we decide on the comparison result)
-// Cannot be an interface because we want the properties to be required on initialization
-public class ParticipantSorter<T>
-{
-    public required Func<ParticipantDto,T> KeySelector { get; set;}
-    public required ISwitchableComparer<T> KeyComparer { get; set;}
-
-}
-
-// Used by div tables to sort the columns and track the corresponding arrows that should be displayed next to the active sorting column
-public class ColumnSortingManager
-{
-    private ParticipantSorter<object>[] _participantSorters;
-
-    private string[] _headerArrows;
-
-    // Getter only property that makes it impossible to assign to the backing field from the outside of this class
-    public IReadOnlyList<string> HeaderArrows => Array.AsReadOnly(_headerArrows);
-
-    // Current index by which we are sorting
-    private int activeColumnIndex;
-
-    // Constructor takes: 
-    //  1. the array of all sorters (combinations of key selectors and key comparers that can reverse direction) 
-    //  2. index of column which I want to have sorted on page initialization
-    public ColumnSortingManager(ParticipantSorter<object>[] participantSorters, int initialActiveColumnIndex)
-    {
-        _participantSorters = participantSorters;
-        activeColumnIndex = initialActiveColumnIndex;
-        _headerArrows = new string[_participantSorters.Length];
-    }
-
-    // The sort click is handled depending if the clicked column was already active or not
-    // we flip the sort direction if the column was already active and was clicked
-    // otherwise we sort in unreversed direction
-    // lastly we have to update our active column
-    public void HandleSortClick(int clickedColumn)
-    {
-        AdjustSorters(clickedColumn);
-        AdjustHeaderArrows(clickedColumn);
-        activeColumnIndex = clickedColumn;
-    }
-
-    public IEnumerable<ParticipantDto> GetSortedParticipants(IEnumerable<ParticipantDto> unsortedParticipants)
-    {
-        return unsortedParticipants.OrderBy(GetActiveSorterKeySelector(),GetActiveSorterKeyComparer());
-    }
-
-    private Func<ParticipantDto,object> GetActiveSorterKeySelector()
-    {
-        return _participantSorters[activeColumnIndex].KeySelector;
-    }
-
-    private ISwitchableComparer<object> GetActiveSorterKeyComparer()
-    {
-        return _participantSorters[activeColumnIndex].KeyComparer;
-    }
-
-    // Adjust the sorter reversed flags based on if we clicked on a new column or not
-    private void AdjustSorters(int clickedColumn)
-    {
-        if (clickedColumn == activeColumnIndex) // clicked on column that was already active
-        {
-            _participantSorters[clickedColumn].KeyComparer.ReverseSort = !_participantSorters[clickedColumn].KeyComparer.ReverseSort;
-        }
-        else // clicked on new column
-        {
-            _participantSorters[clickedColumn].KeyComparer.ReverseSort = false;
-        }
-    }
-
-    // Adjust the markers showing if sorting ascending or descending
-    private void AdjustHeaderArrows(int clickedColumn)
-    {
-        if (clickedColumn != activeColumnIndex) // clicked on a new column, remove the marker from the old one
-        {
-            _headerArrows[activeColumnIndex] = "";
-        }
-        _headerArrows[clickedColumn] = _participantSorters[clickedColumn].KeyComparer.ReverseSort ? "▼" : "▲"; // set current marker depending on if the sorter is reversed
     }
 }
